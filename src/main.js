59a4b635dbd65b6818b4e04d4c937f2bcf7ed94f
ModyQyW <wurui7@mail3.sysu.edu.cn>
2020-09-16 14:17:22 +0800

    fix: fix import

--- src/main.js
+++ src/main.js
@@ -1,11 +1,12 @@
 import Vue from 'vue';
+import 'ress';
+import '@/directives';
+import '@/plugins';
 import i18n from './i18n';
 import router from './router';
 import store from './store';
 import App from './App.vue';
 import '@/styles/app.scss';
-import '@/directives';
-import '@/plugins';
 import '@/guard';
 
 Vue.config.productionTip = false;

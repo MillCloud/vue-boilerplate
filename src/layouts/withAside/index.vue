2ae89aac4a54b380524287e6193ced59ad991bfd
ModyQyW <wurui7@mail3.sysu.edu.cn>
2020-09-24 20:43:59 +0800

    feat: preset simple layouts

---
+++ src/layouts/withAside/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <p>A layout with aside</p>
+    <router-view />
+  </div>
+</template>
+
+<script>
+export default {
+
+};
+</script>
---
+++ src/layouts/withoutAside/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <p>A layout without aside</p>
+    <router-view />
+  </div>
+</template>
+
+<script>
+export default {
+
+};
+</script>

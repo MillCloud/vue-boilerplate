7449237c9f4db933a107ae43b3f84cd1afa7fd82
ModyQyW <wurui7@mail3.sysu.edu.cn>
2020-10-04 16:52:12 +0800

    chore: add issue link

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   plugins: {
     tailwindcss: {},
+    // https://github.com/FullHuman/purgecss/issues/392
     // 'vue-cli-plugin-tailwind/purgecss': {},
     autoprefixer: {},
   },

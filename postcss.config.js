a521436ad1d2756f4b4f82dc973f7bac5256972a
ModyQyW <wurui7@mail3.sysu.edu.cn>
2020-11-22 23:31:40 +0800

    chore: remove tailwindcss

--- postcss.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  plugins: {
-    tailwindcss: {},
-    // https://github.com/FullHuman/purgecss/issues/392
-    // 'vue-cli-plugin-tailwind/purgecss': {},
-    autoprefixer: {},
-  },
-};

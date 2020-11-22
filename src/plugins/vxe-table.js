fbebea25ed2ff5900ffdd046e7fffc32dd054798
ModyQyW <wurui7@mail3.sysu.edu.cn>
2020-11-23 00:12:20 +0800

    chore: remove extra comments

--- src/plugins/vxe-table.js
+++ src/plugins/vxe-table.js
@@ -7,9 +7,6 @@ import '@/styles/vxe-table-variables.scss';
 
 Vue.use(VXETable);
 
-// 将模态窗口挂载到 vue 实例中
-// Vue.prototype.$XModal = VXETable.modal
-
 VXETable.setup({
   // 国际化内置提示语
   i18n: (key) => i18n.t(key),

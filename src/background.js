9cd4dc522dafd242f8e8d584d33bef7074e0d3b1
ModyQyW <wurui7@mail3.sysu.edu.cn>
2020-12-17 15:51:40 +0800

    fix: fix background.js preload path

--- src/background.js
+++ src/background.js
@@ -24,7 +24,7 @@ async function createWindow() {
       // Use pluginOptions.nodeIntegration, leave this alone
       // See nklayman.github.io/vue-cli-plugin-electron-builder/guide/security.html#node-integration for more info
       nodeIntegration: process.env.ELECTRON_NODE_INTEGRATION,
-      preload: path.resolve('src', 'preload.js'),
+      preload: path.join(__dirname, 'preload.js'),
     },
   });
 

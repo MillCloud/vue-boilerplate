849ace2e09c22e575c03fed734774610fd269a76
ModyQyW <wurui7@mail3.sysu.edu.cn>
2021-01-03 02:04:51 +0800

    feat: update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,5 @@
 <template>
-  <v-app>
-    <v-app-bar app>
-      <router-link to="/" exact class="mr-2">Home</router-link>
-      |
-      <router-link to="/about" exact class="ml-2">About</router-link>
-    </v-app-bar>
-    <router-view />
-  </v-app>
+  <router-view />
 </template>
 
 <script>

3eb0ea25b8b9293d9a7c54126f1a295ee37e4c85
ModyQyW <wurui7@mail3.sysu.edu.cn>
2021-01-03 02:05:34 +0800

    feat: update apis

--- src/apis/user.js
+++ src/apis/user.js
@@ -1,19 +1,40 @@
 import request from '@/plugins/request';
+import axios from 'axios';
+
+const { CancelToken } = axios;
 
 export function signIn({ username, password }) {
-  return request('/api/sign-in', {
-    username,
-    password,
+  let abort;
+  const r = request({
+    url: '/api/sign-in',
+    data: {
+      username,
+      password,
+    },
+    cancelToken: new CancelToken((c) => {
+      abort = c;
+    }),
   });
+  r.prototype.abort = abort;
+  return r;
 }
 
 export function signUp({ email, username, nickname, password }) {
-  return request('/api/sign-up', {
-    email,
-    username,
-    nickname,
-    password,
+  let abort;
+  const r = request({
+    url: '/api/sign-up',
+    data: {
+      email,
+      username,
+      nickname,
+      password,
+    },
+    cancelToken: new CancelToken((c) => {
+      abort = c;
+    }),
   });
+  r.prototype.abort = abort;
+  return r;
 }
 
 export default {

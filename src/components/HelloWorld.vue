0bf14393eb0494f6c2bd12f5cb0d8b0287c383f1
ModyQyW <wurui7@mail3.sysu.edu.cn>
2021-01-03 02:03:21 +0800

    feat: update components

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,157 +1,142 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br />
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener noreferrer">
-        vue-cli documentation
-      </a>
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li>
+  <v-row>
+    <v-col cols="12">
+      <h3 class="text-h3 text-center">{{ msg }}</h3>
+    </v-col>
+    <v-col cols="12">
+      <p class="text-center">
+        For a guide and recipes on how to configure / customize this project,
+        check out the
         <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel"
+          href="https://cli.vuejs.org"
           target="_blank"
           rel="noopener noreferrer"
+          >vue-cli documentation</a
         >
-          babel
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          router
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-vuex"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          vuex
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          eslint
-        </a>
-      </li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li>
-        <a href="https://vuejs.org" target="_blank" rel="noopener noreferrer">
-          Core Docs
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://forum.vuejs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Forum
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://chat.vuejs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Community Chat
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://twitter.com/vuejs"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Twitter
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://news.vuejs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          News
-        </a>
-      </li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li>
-        <a
-          href="https://router.vuejs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          vue-router
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://vuex.vuejs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          vuex
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://vueuse.js.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          vue-use
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-devtools#vue-devtools"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          vue-devtools
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://vue-loader.vuejs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          vue-loader
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          awesome-vue
-        </a>
-      </li>
-    </ul>
-  </div>
+      </p>
+    </v-col>
+    <v-col cols="12">
+      <h6 class="text-h6 text-center">Installed CLI Plugins</h6>
+    </v-col>
+    <v-col cols="12" class="text-center">
+      <a
+        class="mx-2"
+        href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel"
+        target="_blank"
+        rel="noopener noreferrer"
+        >babel</a
+      >
+      <a
+        class="mx-2"
+        href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router"
+        target="_blank"
+        rel="noopener noreferrer"
+        >router</a
+      >
+      <a
+        class="mx-2"
+        href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-vuex"
+        target="_blank"
+        rel="noopener noreferrer"
+        >vuex</a
+      >
+      <a
+        class="mx-2"
+        href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint"
+        target="_blank"
+        rel="noopener noreferrer"
+        >eslint</a
+      >
+      <a
+        class="mx-2"
+        href="https://github.com/vuetifyjs/vue-cli-plugins/tree/master/packages/vue-cli-plugin-vuetify#readme"
+        target="_blank"
+        rel="noopener noreferrer"
+        >i18n</a
+      >
+      <a
+        v-if="isElectron"
+        class="mx-2"
+        href="https://github.com/vuetifyjs/vue-cli-plugins/tree/master/packages/vue-cli-plugin-vuetify#readme"
+        target="_blank"
+        rel="noopener noreferrer"
+        >electron-builder</a
+      >
+      <a
+        class="mx-2"
+        href="https://github.com/vuetifyjs/vue-cli-plugins/tree/master/packages/vue-cli-plugin-vuetify#readme"
+        target="_blank"
+        rel="noopener noreferrer"
+        >vuetify</a
+      >
+    </v-col>
+    <v-col cols="12">
+      <h6 class="text-h6 text-center">Ecosystem</h6>
+    </v-col>
+    <v-col cols="12" class="text-center">
+      <a
+        class="mx-2"
+        href="https://cn.vuejs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+        >vue</a
+      >
+      <a
+        class="mx-2"
+        href="https://router.vuejs.org/zh/"
+        target="_blank"
+        rel="noopener noreferrer"
+        >vue-router</a
+      >
+      <a
+        class="mx-2"
+        href="https://vuex.vuejs.org/zh/"
+        target="_blank"
+        rel="noopener noreferrer"
+        >vuex</a
+      >
+      <a
+        class="mx-2"
+        href="https://vueuse.js.org/"
+        target="_blank"
+        rel="noopener noreferrer"
+        >vue-use</a
+      >
+      <a
+        class="mx-2"
+        href="https://github.com/vuejs/vue-devtools#vue-devtools"
+        target="_blank"
+        rel="noopener noreferrer"
+        >vue-devtools</a
+      >
+      <a
+        class="mx-2"
+        href="https://vue-loader.vuejs.org/zh/"
+        target="_blank"
+        rel="noopener noreferrer"
+        >vue-loader</a
+      >
+      <a
+        class="mx-2"
+        href="https://vuetifyjs.com"
+        target="_blank"
+        rel="noopener noreferrer"
+        >vuetify</a
+      >
+      <a
+        class="mx-2"
+        href="https://github.com/vuejs/awesome-vue#readme"
+        target="_blank"
+        rel="noopener noreferrer"
+        >awesome-vue</a
+      >
+    </v-col>
+  </v-row>
 </template>
 
 <script>
+import { mapState } from 'vuex';
+
 export default {
   name: 'HelloWorld',
   props: {
@@ -160,23 +145,24 @@ export default {
       default: () => '',
     },
   },
+  computed: {
+    ...mapState({
+      isElectron: (state) => state.isElectron,
+    }),
+  },
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-h3 {
-  margin: 40px 0 0;
-}
+/* stylelint-disable declaration-no-important */
 ul {
-  padding: 0;
+  padding: 0 !important;
   list-style-type: none;
 }
+
 li {
   display: inline-block;
   margin: 0 10px;
-}
-a {
-  color: #42b983;
 }
 </style>

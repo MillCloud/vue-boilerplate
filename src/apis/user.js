ec73a26f4e63224182544751c063fe5e7581da5b
ModyQyW <wurui7@mail3.sysu.edu.cn>
2021-01-19 00:20:52 +0800

    feat: update request examples

--- src/apis/user.js
+++ src/apis/user.js
@@ -4,9 +4,10 @@ import axios from 'axios';
 // 考虑到取消请求是较为少数的情况，可以这么处理
 const { CancelToken } = axios;
 
-export function signIn({ username, password }) {
+export function signIn({ username, password, showError = true } = {}) {
   let abort;
   const r = request({
+    method: 'POST',
     url: '/api/sign-in',
     data: {
       username,
@@ -15,17 +16,23 @@ export function signIn({ username, password }) {
     cancelToken: new CancelToken((c) => {
       abort = c;
     }),
-    // 不要自动处理错误，手动处理
-    showError: false,
+    showError,
   });
   // 供取消请求
   r.abort = abort;
   return r;
 }
 
-export function signUp({ email, username, nickname, password }) {
+export function signUp({
+  email,
+  username,
+  nickname,
+  password,
+  showError = true,
+} = {}) {
   let abort;
   const r = request({
+    method: 'POST',
     url: '/api/sign-up',
     data: {
       email,
@@ -36,6 +43,21 @@ export function signUp({ email, username, nickname, password }) {
     cancelToken: new CancelToken((c) => {
       abort = c;
     }),
+    showError,
+  });
+  r.abort = abort;
+  return r;
+}
+
+export function renew({ showError = true } = {}) {
+  let abort;
+  const r = request({
+    method: 'POST',
+    url: '/api/renew',
+    cancelToken: new CancelToken((c) => {
+      abort = c;
+    }),
+    showError,
   });
   r.abort = abort;
   return r;
@@ -44,4 +66,5 @@ export function signUp({ email, username, nickname, password }) {
 export default {
   signIn,
   signUp,
+  renew,
 };

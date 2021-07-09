b4b7330cac294fab5c5b81fe89e54f694cdc1034
ModyQyW <wurui7@mail3.sysu.edu.cn>
2021-07-09 15:06:40 +0800

    feat: update request

--- src/plugins/request.js
+++ src/plugins/request.js
@@ -3,6 +3,9 @@ import axios from 'axios';
 import router from '@/router';
 import i18n from '@/i18n';
 import { getToken, clear as clearStorage } from '@/utils';
+import statuses from 'statuses';
+import { constantCase } from '@modyqyw/utils';
+import axiosRetry from 'axios-retry';
 import packageInfo from '../../package.json';
 
 // https://github.com/axios/axios#readme
@@ -10,47 +13,6 @@ import packageInfo from '../../package.json';
 
 /** @desc 需要返回到首页并清空登录信息的响应代码 */
 export const reLaunchCodes = new Set(['TOKEN_OUTDATED']);
-
-/** @desc 状态码对应的国际化键 */
-export const objectStatusCode = {
-  400: 'BAD_REQUEST',
-  401: 'UNAUTHORIZED',
-  403: 'FORBIDDEN',
-  404: 'NOT_FOUND',
-  405: 'METHOD_NOT_ALLOWED',
-  406: 'NOT_ACCEPTABLE',
-  407: 'PROXY_AUTHENTICATION_REQUIRED',
-  408: 'REQUEST_TIMEOUT',
-  409: 'CONFLICT',
-  410: 'GONE',
-  411: 'LENGTH_REQUIRED',
-  412: 'PRECONDITION_FAILED',
-  413: 'PAYLOAD_TOO_LARGE',
-  414: 'URI_TOO_LONG',
-  415: 'UNSUPPORTED_MEDIA_TYPE',
-  416: 'RANGE_NOT_SATISFIABLE',
-  417: 'EXPECTATION_FAILED',
-  421: 'MISDIRECTED_REQUEST',
-  422: 'UNPROCESSABLE_ENTITY',
-  423: 'LOCKED',
-  424: 'FAILED_DEPENDENCY',
-  426: 'UPGRADE_REQUIRED',
-  428: 'PRECONDITION_REQUIRED',
-  429: 'TOO_MANY_REQUESTS',
-  431: 'REQUEST_HEADER_FIELDS_TOO_LARGE',
-  451: 'UNAVAILABLE_FOR_LEGAL_REASONS',
-  500: 'INTERNAL_SERVER_ERROR',
-  501: 'NOT_IMPLEMENTED',
-  502: 'BAD_GATEWAY',
-  503: 'SERVICE_UNAVAILABLE',
-  504: 'GATEWAY_TIMEOUT',
-  505: 'HTTP_VERSION_NOT_SUPPORTED',
-  506: 'VARIANT_ALSO_NEGOTIATES',
-  507: 'INSUFFICIENT_STORAGE',
-  508: 'LOOP_DETECTED',
-  510: 'NOT_EXTENDED',
-  511: 'NETWORK_AUTHENTICATION_REQUIRED',
-};
 
 /** @param {number} statusCode */
 const handleValidateStatusCode = (statusCode) =>
@@ -89,6 +51,8 @@ instance.interceptors.request.use((config) => ({
   },
 }));
 
+axiosRetry(instance, { retryDelay: axiosRetry.exponentialDelay });
+
 instance.interceptors.response.use(
   (response) => {
     const { data, config } = response;
@@ -118,8 +82,8 @@ instance.interceptors.response.use(
         // 状态码不正常
         status = JSON.stringify(status);
         response.code = status;
-        response.message = objectStatusCode[status]
-          ? i18n.t(`error.${objectStatusCode[status]}`)
+        response.message = statuses(status)
+          ? i18n.t(`error.${constantCase(statuses(status))}`)
           : i18n.t('error.ERROR_OCCURRED');
       } else {
         // 超时

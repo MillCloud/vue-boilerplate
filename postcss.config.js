98a7d4bc9651f6d964ef47b68ec796c9676f40b6
ModyQyW <wurui7@mail3.sysu.edu.cn>
2021-08-31 10:08:05 +0800

    chore: update tailwindcss config, using 1 but with 2 experience

--- postcss.config.js
+++ postcss.config.js
@@ -2,7 +2,7 @@
 module.exports = {
   plugins: [
     require('tailwindcss'),
-    require('autoprefixer'),
+    require('postcss-preset-env'),
     process.env.NODE_ENV === 'production' &&
       require('@fullhuman/postcss-purgecss')({
         content: [`./public/**/*.html`, `./src/**/*.vue`],

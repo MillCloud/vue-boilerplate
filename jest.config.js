8000c7dc08d592c80d9a5e766f99aeb784f4903b
ModyQyW <wurui7@mail3.sysu.edu.cn>
2021-09-10 14:57:28 +0800

    chore: update jest config

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,9 @@ module.exports = {
   preset: '@vue/cli-plugin-unit-jest',
   moduleNameMapper: {
     '@/(.*)$': '<rootDir>/src/$1',
+    '@@/(.*)$': '<rootDir>/$1',
+  },
+  transform: {
+    '.*\\.(vue)$': 'unplugin-vue2-script-setup/jest',
   },
 };

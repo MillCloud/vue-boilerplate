d0433a68c857022948259312965c6f6a46c5ecc0
ModyQyW <wurui7@mail3.sysu.edu.cn>
2021-09-30 20:30:04 +0800

    feat: update tailwind styles

--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,8 @@ import router from './router';
 import store from './store';
 import App from './App.vue';
 import '@/styles/preflight.scss';
-import '@/styles/tailwind.scss';
 import '@/styles/global.scss';
+import '@/styles/tailwind.scss';
 import '@/guard';
 
 enableAllPlugins();

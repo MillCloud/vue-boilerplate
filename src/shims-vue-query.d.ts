1bc7e246c96604ddf6662a6b8b0fc283724e0657
ModyQyW <wurui7@mail3.sysu.edu.cn>
2021-11-17 16:38:43 +0800

    chore: update dts

--- src/shims-vue-query.d.ts
+++ src/shims-vue-query.d.ts
@@ -1,21 +1,27 @@
 import 'vue-query';
-import { AxiosRequestConfig } from 'axios';
-import type {
-  UseInfiniteQueryOptions,
-  UseQueryOptions,
-  UseMutationOptions,
-  UseMutationReturnType,
-} from 'vue-query';
-import type { UseQueryReturnType } from 'vue-query/lib/vue/useBaseQuery';
+import type { AxiosRequestConfig } from 'axios';
 import type {
   QueryFunction,
   QueryKey,
   MutationFunction,
   MutationKey,
 } from 'react-query/types/core';
-import type { UseInfiniteQueryResult } from 'react-query/types/react/types';
+import type {
+  UseQueryOptions as UQO,
+  UseQueryResult,
+  UseInfiniteQueryResult,
+} from 'react-query/types/react/types';
+import type { UseInfiniteQueryOptions, UseMutationOptions, UseMutationReturnType } from 'vue-query';
+import type { WithQueryClientKey } from 'vue-query/lib/vue/types';
+import type { UseQueryReturnType } from 'vue-query/lib/vue/useBaseQuery';
 
 declare module 'vue-query' {
+  export declare type UseQueryOptions<
+    TQueryFnData = IResponseData,
+    TError = IResponseError,
+    TData = TQueryFnData
+  > = WithQueryClientKey<UQO<TQueryFnData, TError, TData>>;
+
   export declare function useInfiniteQuery<
     TQueryFnData = IResponseData,
     TError = IResponseError,
@@ -94,6 +100,10 @@ declare module 'vue-query' {
     mutationFn?: MutationFunction<TData, TVariables>,
     options?: UseMutationOptions<TData, TError, TVariables, TContext>,
   ): UseMutationReturnType<TData, TError, TVariables, TContext>;
+
+  export declare function useQueries(
+    queries: UseQueryOptions<IResponseData, IResponseError>[],
+  ): Readonly<UseQueryResult<IResponseData, IResponseError>[]>;
 
   export declare function useQuery<
     TQueryFnData = IResponseData,

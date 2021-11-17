84a0158ab3ea34124b5eceb6adcea520dc03646f
ModyQyW <wurui7@mail3.sysu.edu.cn>
2021-11-17 17:17:15 +0800

    feat: update default

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,7 +1,7 @@
 <template>
   <el-container class="relative min-h-screen bg-gray-4">
     <el-aside
-      class="fixed z-20 flex flex-col w-64 h-screen transition border-0 border-r border-solid border-gray-5"
+      class="fixed z-20 flex flex-col w-64 h-screen transition bg-white border-0 border-r border-solid border-gray-5"
       :style="{
         width: !isAsideCollapsed ? '256px' : '64px',
       }"
@@ -9,7 +9,7 @@
       <el-header class="flex items-center flex-none w-full p-0 transition">
         <img src="@/assets/app.png" class="h-full" alt="logo" title="logo" />
         <span v-show="!isAsideCollapsed" class="text-xl font-bold transition">
-          {{ pkg.name }}
+          {{ pkg.description || pkg.name }}
         </span>
       </el-header>
       <el-menu
@@ -75,7 +75,9 @@
       </el-footer>
     </el-aside>
     <el-container class="relative flex-col min-h-screen" :class="{ 'is-wider': isAsideCollapsed }">
-      <el-header class="fixed z-10 flex-none w-full transition bg-white">
+      <el-header
+        class="fixed z-10 flex-none w-full transition bg-white border-0 border-b border-solid border-gray-5"
+      >
         <el-row class="flex items-center justify-between w-full h-full">
           <el-col class="flex-auto w-auto">
             <el-breadcrumb>
@@ -97,104 +99,89 @@
   </el-container>
 </template>
 
-<script lang="ts">
-import { defineComponent, ref, computed } from '@vue/composition-api';
+<script setup lang="ts">
+import { ref, computed } from '@vue/composition-api';
 import { useStore } from 'vue2-helpers/vuex';
 import { useRouter } from 'vue2-helpers/vue-router';
 import { Icon } from '@iconify/vue2';
+import { useRoute } from '@/composables';
 import pkg from '@/../package.json';
 import { getIsAsideCollapsed, setIsAsideCollapsed } from '@/utils';
 
-export default defineComponent({
-  components: {
-    Icon,
-  },
-  setup(props, { root }) {
-    const store = useStore();
-    const user = computed(() => store.state.user);
+const store = useStore();
+const user = computed(() => store.state.user);
 
-    const router = useRouter();
-    // 不考虑多层路由，多层路由往往意味着路由设计有问题，应该重新设计路由
-    const menuItems = computed(() =>
-      (router.options.routes?.[0]?.children ?? [])
-        // isHidden 或 isChildrenHidden 隐藏
-        .filter((item) => (item?.meta?.isHidden ?? false) !== true)
-        .map((parent) => ({
-          ...parent,
-          children: parent?.meta?.isChildrenHidden ?? false ? [] : parent?.children ?? [],
-        }))
-        .map((parent) => ({
-          ...parent,
-          children: [...(parent.children ?? [])].filter((child) => child?.meta?.isHidden !== true),
-        }))
-        // 限定角色
-        .filter((item) => (item?.meta?.roles ?? [0, 3, 5]).includes(user.value.role))
-        .map((parent) => ({
-          ...parent,
-          children: [...(parent.children ?? [])].filter((child) =>
-            (child?.meta?.roles ?? [0, 3, 5]).includes(user.value.role),
-          ),
-        }))
-        // 排序，sort 越小，显示越靠前，默认 0，相同的 sort 按照字典顺序排序
-        .sort((itemA, itemB) => (itemA?.meta?.sort ?? 0) - (itemB?.meta?.sort ?? 0))
-        .map((parent) => ({
-          ...parent,
-          children: [...(parent.children ?? [])].sort(
-            (childA, childB) => (childA?.meta?.sort ?? 0) - (childB?.meta?.sort ?? 0),
-          ),
-        }))
-        // 补全路径
-        .map((item) => ({
-          ...item,
-          path: `/${item.path}`,
-        }))
-        .map((parent) => ({
-          ...parent,
-          children: [...(parent.children ?? [])].map((child) => ({
-            ...child,
-            path: child.path === '' ? parent.path : `${parent.path}/${child.path}`,
-          })),
-        })),
-    );
+const router = useRouter();
+const route = useRoute();
+// 不考虑多层路由，多层路由往往意味着路由设计有问题，应该重新设计路由
+const menuItems = computed(() =>
+  (router.options.routes?.[0]?.children ?? [])
+    // isHidden 或 isChildrenHidden 隐藏
+    .filter((item) => (item?.meta?.isHidden ?? false) !== true)
+    .map((parent) => ({
+      ...parent,
+      children: parent?.meta?.isChildrenHidden ?? false ? [] : parent?.children ?? [],
+    }))
+    .map((parent) => ({
+      ...parent,
+      children: [...(parent.children ?? [])].filter((child) => child?.meta?.isHidden !== true),
+    }))
+    // 限定角色
+    .filter((item) => (item?.meta?.roles ?? [0, 3, 5]).includes(user.value.role))
+    .map((parent) => ({
+      ...parent,
+      children: [...(parent.children ?? [])].filter((child) =>
+        (child?.meta?.roles ?? [0, 3, 5]).includes(user.value.role),
+      ),
+    }))
+    // 排序，sort 越小，显示越靠前，默认 0，相同的 sort 按照字典顺序排序
+    .sort((itemA, itemB) => (itemA?.meta?.sort ?? 0) - (itemB?.meta?.sort ?? 0))
+    .map((parent) => ({
+      ...parent,
+      children: [...(parent.children ?? [])].sort(
+        (childA, childB) => (childA?.meta?.sort ?? 0) - (childB?.meta?.sort ?? 0),
+      ),
+    }))
+    // 补全路径
+    .map((item) => ({
+      ...item,
+      path: `/${item.path}`,
+    }))
+    .map((parent) => ({
+      ...parent,
+      children: [...(parent.children ?? [])].map((child) => ({
+        ...child,
+        path: child.path === '' ? parent.path : `${parent.path}/${child.path}`,
+      })),
+    })),
+);
 
-    const defaultActive = computed(() => {
-      const menuItem = menuItems.value.find(
-        (item) => item.path.includes(root.$route.path) || root.$route.path.includes(item.path),
-      );
-      if (!menuItem) {
-        return '';
-      }
-      if (menuItem.meta?.isChildrenHidden) {
-        return menuItem.path;
-      }
-      const menuItemChild = menuItem.children.find((child) => child.path === root.$route.path);
-      return menuItemChild ? menuItemChild.path : menuItem.path;
-    });
-
-    const breadcrumbs = computed(() =>
-      root.$route.matched
-        .slice(1)
-        .filter((item) => (item?.meta?.isHidden ?? false) !== true)
-        .filter((item) => item?.meta?.name),
-    );
-
-    const isAsideCollapsed = ref(getIsAsideCollapsed());
-    const handleToggleIsAsideCollapsed = () => {
-      isAsideCollapsed.value = !isAsideCollapsed.value;
-      setIsAsideCollapsed(isAsideCollapsed.value);
-    };
-
-    return {
-      pkg,
-      menuItems,
-      defaultActive,
-      breadcrumbs,
-      isAsideCollapsed,
-      handleToggleIsAsideCollapsed,
-      user,
-    };
-  },
+const defaultActive = computed(() => {
+  const menuItem = menuItems.value.find(
+    (item) => item.path.includes(route.value.path) || route.value.path.includes(item.path),
+  );
+  if (!menuItem) {
+    return '';
+  }
+  if (menuItem.meta?.isChildrenHidden) {
+    return menuItem.path;
+  }
+  const menuItemChild = menuItem.children.find((child) => child.path === route.value.path);
+  return menuItemChild ? menuItemChild.path : menuItem.path;
 });
+
+const breadcrumbs = computed(() =>
+  route.value.matched
+    .slice(1)
+    .filter((item) => (item?.meta?.isHidden ?? false) !== true)
+    .filter((item) => item?.meta?.name),
+);
+
+const isAsideCollapsed = ref(getIsAsideCollapsed());
+const handleToggleIsAsideCollapsed = () => {
+  isAsideCollapsed.value = !isAsideCollapsed.value;
+  setIsAsideCollapsed(isAsideCollapsed.value);
+};
 </script>
 
 <style scoped lang="scss">

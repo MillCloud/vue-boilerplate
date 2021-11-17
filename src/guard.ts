7b821831c4653bf6974f0c00f9f4ebfcb67d8a52
ModyQyW <wurui7@mail3.sysu.edu.cn>
2021-11-17 17:04:39 +0800

    chore: update guard example

--- src/guard.ts
+++ src/guard.ts
@@ -1,5 +1,6 @@
 import nprogress from 'nprogress';
 import router from './router';
+// import { getToken } from './utils';
 // import store from './store';
 
 router.beforeEach(async (to, from, next) => {
@@ -11,6 +12,62 @@ router.beforeEach(async (to, from, next) => {
   // }
   // 启动 nprogress
   nprogress.start();
+
+  // const token = getToken();
+
+  // // 放行所有前往首页的路由跳转
+  // if (to.path === '/') {
+  //   if (token === '') {
+  //     next('/sign-in');
+  //     return;
+  //   }
+
+  //   next();
+  //   return;
+  // }
+
+  // // 对于前往登录页的路由跳转，只放行没有登录态的情况
+  // if (to.path === '/sign-in') {
+  //   if (token === '') {
+  //     next();
+  //     return;
+  //   }
+
+  //   next('/');
+  //   return;
+  // }
+
+  // // 对于需要授权的路由跳转，只放行有登录态且有用户信息的情况
+  // if (to.meta?.requiresAuth !== false) {
+  //   if (token !== '' && store.state.user.id !== 0) {
+  //     next();
+  //     return;
+  //   }
+
+  //   if (token !== '' && store.state.user.id === 0) {
+  //     next({
+  //       path: '/',
+  //       query: {
+  //         redirect: encodeURIComponent(to.fullPath),
+  //       },
+  //     });
+  //     return;
+  //   }
+
+  //   if (token === '') {
+  //     next({
+  //       path: '/sign-in',
+  //       query: {
+  //         redirect: encodeURIComponent(to.fullPath),
+  //       },
+  //     });
+  //     return;
+  //   }
+
+  //   next();
+  //   return;
+  // }
+
   next();
 });
 

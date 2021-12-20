9d3e96eb98df3e20957ac609f74cc58566329193
ModyQyW <wurui7@mail3.sysu.edu.cn>
2021-12-20 09:31:31 +0800

    chore: update config

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,6 @@ import router from './router';
 import App from './App.vue';
 import '@/styles/preflight.scss';
 import '@/styles/global.scss';
-import '@/styles/tailwind.scss';
 import '@/guard';
 
 const app = new Vue({

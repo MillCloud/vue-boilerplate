03ed60d47400d9043e8b341b62c030ca0c240109
ModyQyW <wurui7@mail3.sysu.edu.cn>
2022-01-19 13:36:48 +0800

    perf: remove custom useRoute

--- src/composables/useRoute.ts
+++
@@ -1,9 +0,0 @@
-import { getCurrentInstance, computed } from '@vue/composition-api';
-
-export function useRoute() {
-  const vm = getCurrentInstance();
-  if (!vm) {
-    throw new Error('useRoute() must be called in setup.');
-  }
-  return computed(() => vm.proxy.$route);
-}

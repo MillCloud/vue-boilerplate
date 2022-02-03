0a0298ed2714419cfb76af92fa26403b9d65120b
ModyQyW <wurui7@mail3.sysu.edu.cn>
2022-02-03 14:02:16 +0800

    fix: fix order

--- src/router/index.ts
+++ src/router/index.ts
@@ -5,7 +5,18 @@ import generatedRoutes from 'virtual:generated-pages';
 
 Vue.use(VueRouter);
 
-const routes = setupLayouts(generatedRoutes);
+const routes = setupLayouts(
+  generatedRoutes.sort((itemA, itemB) => {
+    if (itemA.name === 'all') {
+      return 1;
+    }
+    if (itemB.name === 'all') {
+      return -1;
+    }
+    return 0;
+  }),
+);
+
 export default new VueRouter({
   mode: 'hash',
   scrollBehavior: () => ({ x: 0, y: 0, behavior: 'smooth' }),
